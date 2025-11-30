3154d1b17dce199bfde62c61587cf78621497bd2
AbeKhan <abrahammkhan@gmail.com>
2025-11-30 17:08:36 -0600

    Add erase and leaf_count to BST, improve main menu
    Implemented erase and leaf_count methods in BinaryTree, updated main.cpp to load employees from file, and expanded the interactive menu to support insertion, removal, clear, traversals, and leaf count. Added a zip target to the Makefile and updated .vscode settings. Removed generated binaries and object files.

--- A5 BST with Employee Class attached files Nov 29, 2025 937 PM/A5 BST with Employee Class/BinaryTree.h
+++ A5 BST with Employee Class attached files Nov 29, 2025 937 PM/A5 BST with Employee Class/BinaryTree.h
@@ -11,45 +11,50 @@ template <typename Key, typename Object>
 class BinaryTree
 {
 public:
-    BinaryTree();
-    bool empty() const;
-    void preorder(void (*visit)(Object &));
-    void inorder(void (*visit)(Object &));
-    void postorder(void (*visit)(Object &));
+	BinaryTree();
+	bool empty() const;
+	void preorder(void (*visit)(Object &));
+	void inorder(void (*visit)(Object &));
+	void postorder(void (*visit)(Object &));
 
-    int size() const;
-    void clear();
-    int height() const;
+	int size() const;
+	void clear();
+	int height() const;
 
-    void print() const; // A method to print out the tree using preorder
+	void print() const; // A method to print out the tree using preorder
 
-    // functions added
-    void insert(const Key &key, const Object &obj);
+	// functions added
+	void insert(const Key &key, const Object &obj);
+	void erase(const Key &key);
+	int leaf_count() const;
 
 	
 
 	// Big Three: copy constructor, operator=(), destructor
-    BinaryTree(const BinaryTree<Key, Object> &original);
-    BinaryTree<Key, Object> &operator=(const BinaryTree<Key, Object> &original);
-    ~BinaryTree();
+	BinaryTree(const BinaryTree<Key, Object> &original);
+	BinaryTree<Key, Object> &operator=(const BinaryTree<Key, Object> &original);
+	~BinaryTree();
 
 protected:
-    // Auxiliary functions
-    void recursive_preorder(BinaryNode<Key, Object> *sub_root, void (*visit)(Object &));
-    void recursive_inorder(BinaryNode<Key, Object> *sub_root, void (*visit)(Object &));
-    void recursive_postorder(BinaryNode<Key, Object> *sub_root, void (*visit)(Object &));
-    int recursive_size(BinaryNode<Key, Object> *sub_root) const;
-    void recursive_clear(BinaryNode<Key, Object> *sub_root);
-    int recursive_height(BinaryNode<Key, Object> *sub_root) const;
-    BinaryNode<Key, Object> *recursive_copy(BinaryNode<Key, Object> *sub_root);
+	// Auxiliary functions
+	void recursive_preorder(BinaryNode<Key, Object> *sub_root, void (*visit)(Object &));
+	void recursive_inorder(BinaryNode<Key, Object> *sub_root, void (*visit)(Object &));
+	void recursive_postorder(BinaryNode<Key, Object> *sub_root, void (*visit)(Object &));
+	int recursive_size(BinaryNode<Key, Object> *sub_root) const;
+	void recursive_clear(BinaryNode<Key, Object> *sub_root);
+	int recursive_height(BinaryNode<Key, Object> *sub_root) const;
+	BinaryNode<Key, Object> *recursive_copy(BinaryNode<Key, Object> *sub_root);
 
-    void recursive_preorder_print_node(BinaryNode<Key, Object> *sub_root) const;
+	void recursive_preorder_print_node(BinaryNode<Key, Object> *sub_root) const;
 
-    // Functions added
-    BinaryNode<Key, Object> *insert(BinaryNode<Key, Object> *r, const Key key, const Object &obj);
+	// Functions added
+	BinaryNode<Key, Object> *insert(BinaryNode<Key, Object> *sub_root, const Key key, const Object &obj);
+	BinaryNode<Key, Object> *erase(BinaryNode<Key, Object> *sub_root, const Key &key);
+	int leaf_count(BinaryNode<Key, Object> *sub_root) const;
+	
 
-    // Data member
-    BinaryNode<Key, Object> *root;
+	// Data member
+	BinaryNode<Key, Object> *root;
 };
 
 // Constructor
@@ -60,212 +65,287 @@ BinaryTree<Key, Object>::BinaryTree() : root(nullptr) {}
 template <typename Key, typename Object>
 BinaryTree<Key, Object>::BinaryTree(const BinaryTree<Key, Object> &original)
 {
-    root = recursive_copy(original.root);
+	root = recursive_copy(original.root);
 }
 
 // Assignment operator
 template <typename Key, typename Object>
 BinaryTree<Key, Object> &BinaryTree<Key, Object>::operator=(const BinaryTree<Key, Object> &original)
 {
-    if (this != &original)
-    {
-        clear();
-        root = recursive_copy(original.root);
-    }
-    return *this;
+	if (this != &original)
+	{
+		clear();
+		root = recursive_copy(original.root);
+	}
+	return *this;
 }
 
 // Destructor
 template <typename Key, typename Object>
 BinaryTree<Key, Object>::~BinaryTree()
 {
-    clear();
+	clear();
 }
 
 // Empty check
 template <typename Key, typename Object>
 bool BinaryTree<Key, Object>::empty() const
 {
-    return root == nullptr;
+	return root == nullptr;
 }
 
 // Traversals
 template <typename Key, typename Object>
 void BinaryTree<Key, Object>::preorder(void (*visit)(Object &))
 {
-    recursive_preorder(root, visit);
+	recursive_preorder(root, visit);
 }
 
 template <typename Key, typename Object>
 void BinaryTree<Key, Object>::inorder(void (*visit)(Object &))
 {
-    recursive_inorder(root, visit);
+	recursive_inorder(root, visit);
 }
 
 template <typename Key, typename Object>
 void BinaryTree<Key, Object>::postorder(void (*visit)(Object &))
 {
-    recursive_postorder(root, visit);
+	recursive_postorder(root, visit);
 }
 
 // Size
 template <typename Key, typename Object>
 int BinaryTree<Key, Object>::size() const
 {
-    return recursive_size(root);
+	return recursive_size(root);
 }
 
 // Clear
 template <typename Key, typename Object>
 void BinaryTree<Key, Object>::clear()
 {
-    recursive_clear(root);
-    root = nullptr;
+	recursive_clear(root);
+	root = nullptr;
 }
 
 // Height
 template <typename Key, typename Object>
 int BinaryTree<Key, Object>::height() const
 {
-    return recursive_height(root);
+	return recursive_height(root);
 }
 
 // Print
 template <typename Key, typename Object>
 void BinaryTree<Key, Object>::print() const
 {
-    cout << endl;
-    cout << setw(38) << setfill('+') << " " << endl;
-    if (root == nullptr)
-        cout << "EMPTY TREE" << endl;
-    else
-    {
-        if (root->left == nullptr && root->right == nullptr)
-            cout << setw(12) << setfill(' ') << root->key << ":" << setw(12) << " " << right << setw(14) << " " << endl;
-        else
-            recursive_preorder_print_node(root);
-    }
-    cout << setw(38) << setfill('+') << " " << endl;
-    cout << setfill(' ') << endl;
+	cout << endl;
+	cout << setw(38) << setfill('+') << " " << endl;
+	if (root == nullptr)
+		cout << "EMPTY TREE" << endl;
+	else
+	{
+		if (root->left == nullptr && root->right == nullptr)
+			cout << setw(12) << setfill(' ') << root->key << ":" << setw(12) << " " << right << setw(14) << " " << endl;
+		else
+			recursive_preorder_print_node(root);
+	}
+	cout << setw(38) << setfill('+') << " " << endl;
+	cout << setfill(' ') << endl;
 }
 
 // Recursive functions
 template <typename Key, typename Object>
 void BinaryTree<Key, Object>::recursive_preorder(BinaryNode<Key, Object> *sub_root, void (*visit)(Object &))
 {
-    if (sub_root != nullptr)
-    {
-        visit(sub_root->object);
-        recursive_preorder(sub_root->left, visit);
-        recursive_preorder(sub_root->right, visit);
-    }
+	if (sub_root != nullptr)
+	{
+		visit(sub_root->obj);
+		recursive_preorder(sub_root->left, visit);
+		recursive_preorder(sub_root->right, visit);
+	}
 }
 
 template <typename Key, typename Object>
 void BinaryTree<Key, Object>::recursive_inorder(BinaryNode<Key, Object> *sub_root, void (*visit)(Object &))
 {
-    if (sub_root != nullptr)
-    {
-        recursive_inorder(sub_root->left, visit);
-        visit(sub_root->object);
-        recursive_inorder(sub_root->right, visit);
-    }
+	if (sub_root != nullptr)
+	{
+		recursive_inorder(sub_root->left, visit);
+		visit(sub_root->obj);
+		recursive_inorder(sub_root->right, visit);
+	}
 }
 
 template <typename Key, typename Object>
 void BinaryTree<Key, Object>::recursive_postorder(BinaryNode<Key, Object> *sub_root, void (*visit)(Object &))
 {
-    if (sub_root != nullptr)
-    {
-        recursive_postorder(sub_root->left, visit);
-        recursive_postorder(sub_root->right, visit);
-        visit(sub_root->object);
-    }
+	if (sub_root != nullptr)
+	{
+		recursive_postorder(sub_root->left, visit);
+		recursive_postorder(sub_root->right, visit);
+		visit(sub_root->obj);
+	}
 }
 
 template <typename Key, typename Object>
 int BinaryTree<Key, Object>::recursive_size(BinaryNode<Key, Object> *sub_root) const
 {
-    if (sub_root == nullptr) return 0;
-    return 1 + recursive_size(sub_root->left) + recursive_size(sub_root->right);
+	if (sub_root == nullptr)
+		return 0;
+	return 1 + recursive_size(sub_root->left) + recursive_size(sub_root->right);
 }
 
 template <typename Key, typename Object>
 void BinaryTree<Key, Object>::recursive_clear(BinaryNode<Key, Object> *sub_root)
 {
-    if (sub_root != nullptr)
-    {
-        recursive_clear(sub_root->left);
-        recursive_clear(sub_root->right);
-        delete sub_root;
-    }
+	if (sub_root != nullptr)
+	{
+		recursive_clear(sub_root->left);
+		recursive_clear(sub_root->right);
+		delete sub_root;
+	}
 }
 
 template <typename Key, typename Object>
 int BinaryTree<Key, Object>::recursive_height(BinaryNode<Key, Object> *sub_root) const
 {
-    if (sub_root == nullptr) return 0;
-    int leftHeight = recursive_height(sub_root->left);
-    int rightHeight = recursive_height(sub_root->right);
-    return 1 + max(leftHeight, rightHeight);
+	if (sub_root == nullptr)
+		return 0;
+	int leftHeight = recursive_height(sub_root->left);
+	int rightHeight = recursive_height(sub_root->right);
+	return 1 + max(leftHeight, rightHeight);
 }
 
 template <typename Key, typename Object>
 BinaryNode<Key, Object> *BinaryTree<Key, Object>::recursive_copy(BinaryNode<Key, Object> *sub_root)
 {
-    if (sub_root == nullptr) return nullptr;
-    BinaryNode<Key, Object> *newNode = new BinaryNode<Key, Object>(sub_root->key, sub_root->object);
-    newNode->left = recursive_copy(sub_root->left);
-    newNode->right = recursive_copy(sub_root->right);
-    return newNode;
+	if (sub_root == nullptr)
+		return nullptr;
+	BinaryNode<Key, Object> *newNode = new BinaryNode<Key, Object>(sub_root->key, sub_root->object);
+	newNode->left = recursive_copy(sub_root->left);
+	newNode->right = recursive_copy(sub_root->right);
+	return newNode;
 }
 
 template <typename Key, typename Object>
 void BinaryTree<Key, Object>::recursive_preorder_print_node(BinaryNode<Key, Object> *sub_root) const
 {
-    if (sub_root != nullptr)
-    {
-        if (sub_root->left != nullptr || sub_root->right != nullptr)
-        {
-            cout << setw(12) << setfill(' ') << sub_root->key << ":";
-            if (sub_root->left != nullptr)
-                cout << setw(12) << setfill(' ') << sub_root->left->key;
-            else
-                cout << setw(12) << " ";
-            if (sub_root->right != nullptr)
-                cout << setw(12) << setfill(' ') << sub_root->right->key;
-            else
-                cout << setw(12) << " ";
-            cout << endl;
-        }
-        recursive_preorder_print_node(sub_root->left);
-        recursive_preorder_print_node(sub_root->right);
-    }
+	if (sub_root != nullptr)
+	{
+		if (sub_root->left != nullptr || sub_root->right != nullptr)
+		{
+			cout << setw(12) << setfill(' ') << sub_root->key << ":";
+			if (sub_root->left != nullptr)
+				cout << setw(12) << setfill(' ') << sub_root->left->key;
+			else
+				cout << setw(12) << " ";
+			if (sub_root->right != nullptr)
+				cout << setw(12) << setfill(' ') << sub_root->right->key;
+			else
+				cout << setw(12) << " ";
+			cout << endl;
+		}
+		recursive_preorder_print_node(sub_root->left);
+		recursive_preorder_print_node(sub_root->right);
+	}
 }
 
 // Insert
 template <typename Key, typename Object>
 void BinaryTree<Key, Object>::insert(const Key &key, const Object &obj)
 {
-    root = insert(root, key, obj);
+	root = insert(root, key, obj);
 }
 
 template <typename Key, typename Object>
-BinaryNode<Key, Object> *BinaryTree<Key, Object>::insert(BinaryNode<Key, Object> *r, const Key key, const Object &obj)
+BinaryNode<Key, Object> *BinaryTree<Key, Object>::insert(BinaryNode<Key, Object> *sub_root, const Key key, const Object &obj)
 {
-    if (r == nullptr)
-    {
-        r = new BinaryNode<Key, Object>(key, obj);
-    }
-    else if (key < r->key)
-    {
-        r->left = insert(r->left, key, obj);
-    }
-    else if (key > r->key)
-    {
-        r->right = insert(r->right, key, obj);
-    }
-    return r;
+	if (sub_root == nullptr)
+	{
+		sub_root = new BinaryNode<Key, Object>(key, obj);
+	}
+	else if (key < sub_root->key)
+	{
+		sub_root->left = insert(sub_root->left, key, obj);
+	}
+	else if (key > sub_root->key)
+	{
+		sub_root->right = insert(sub_root->right, key, obj);
+	}
+	return sub_root;
+}
+
+// Public erase
+template <typename Key, typename Object>
+void BinaryTree<Key, Object>::erase(const Key &key)
+{
+	if (root != nullptr)
+	{
+		root = erase(root, key);
+	}
+}
+
+// Private recursive erase
+template <typename Key, typename Object>
+BinaryNode<Key, Object> *BinaryTree<Key, Object>::erase(BinaryNode<Key, Object> *sub_root, const Key &key)
+{
+	if (sub_root == nullptr)
+	{
+		return nullptr;
+	}
+
+	if (key < sub_root->key)
+	{
+		sub_root->left = erase(sub_root->left, key);
+	}
+	else if (key > sub_root->key)
+	{
+		sub_root->right = erase(sub_root->right, key);
+	}
+	else // key == sub_root->key
+	{
+		// Case: two children
+		if (sub_root->left != nullptr && sub_root->right != nullptr)
+		{
+			// find minimum in right subtree
+			BinaryNode<Key, Object>* minNode = sub_root->right;
+			while (minNode->left != nullptr)
+				minNode = minNode->left;
+
+			sub_root->key = minNode->key;
+			sub_root->obj = minNode->obj;
+			sub_root->right = erase(sub_root->right, minNode->key);
+		}
+		else
+		{
+			// Case: one child or leaf
+			BinaryNode<Key, Object> *discard = sub_root;
+			sub_root = (sub_root->left != nullptr) ? sub_root->left : sub_root->right;
+			delete discard;
+		}
+	}
+
+	return sub_root;
+}
+
+template <typename Key, typename Object>
+int BinaryTree<Key, Object>::leaf_count() const
+{
+	return leaf_count(root);
+}
+
+template <typename Key, typename Object>
+int BinaryTree<Key, Object>::leaf_count(BinaryNode<Key, Object> *sub_root) const
+{
+	if (sub_root == nullptr)
+		return 0;
+	else if (sub_root->left == nullptr && sub_root->right == nullptr)
+		return 1;
+	else
+	{
+		return leaf_count(sub_root->left) + leaf_count(sub_root->right);
+	}
 }
 
 #endif
--- A5 BST with Employee Class attached files Nov 29, 2025 937 PM/A5 BST with Employee Class/main.cpp
+++ A5 BST with Employee Class attached files Nov 29, 2025 937 PM/A5 BST with Employee Class/main.cpp
@@ -1,129 +1,212 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
-
+#include <vector>
 #include "BinaryTree.h"
-// Please change enum.h to ErrorCode.h for a better naming
-#include "ErrorCode.h"
 #include "Employee.h"
 
-// template <typename T>
-// void increment(T data) // template free function
-// {
-//     data = data + 1;
-// }
+using namespace std;
 
 void menu();
-int main()
+
+void printEmployee(Employee &e)
 {
-    BinaryTree<string, Employee> bst; // BinarySearchTree<string>, Employee> bst
-    bool exit_now = false;
-    while (!exit_now)
-    {
-        menu();
-        string input;
-        cin >> input;
-        if (input == "i") // insert mode to get multiple values to insert until the user enters q
-        {
+	cout << e << endl; 
+}
 
-            string value; // empty string
-            while (value != "q")
-            {
-                Employee temp;
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		cerr << "Usage: " << argv[0] << " <filename>" << endl;
+		return 1;
+	}
 
-                temp.setEmpCode("S");
+	string filename = argv[1];
+	ifstream file(filename);
+	if (!file)
+	{
+		cerr << "Error opening file: " << filename << endl;
+		return 1;
+	}
 
-                cout << "Enter an SSN : ";
-                cin >> value;
-                temp.setSSN(value);
+	BinaryTree<string, Employee> bst;
 
-                string first, last;
-                cout << "Enter an first name: ";
-                cin >> first;
+	string line;
+	while (getline(file, line))
+	{
+		if (line.empty())
+			continue;
 
-                cout << "Enter an last name: ";
-                cin >> last;
-                temp.setName(first, last);
+		// Split line into tokens
+		stringstream ss(line);
+		vector<string> fields;
+		string field;
+		while (ss >> field)
+		{
+			fields.push_back(field);
+		}
 
-                cout << "Enter an department: ";
-                cin >> value;
-                temp.setDept(value);
+		// Construct Employee from vector<string>
+		Employee temp(fields);
 
-                cout << "Enter an role: ";
-                cin >> value;
-                temp.setRole(value);
+		// Insert into BST using SSN as key
+		bst.insert(temp.getSSN(), temp);
+	}
 
-                cout << "Enter an salary: ";
-                cin >> value;
-                temp.setSalary(stoi(value));
+	file.close();
 
-				
+	// Show results
+	cout << "Loaded " << bst.size() << " employees from file." << endl;
+	//cout << "Inorder traversal:" << endl;
+	bst.print();
 
-                if (value != "q")
-                {
-                    bst.insert(temp.getSSN(), temp);
-                    bst.print();
-                }
+	bool exit_now = false;
+	while (!exit_now)
+	{
+		menu();
+		string input;
+		cin >> input;
+		if (input == "i") // insert mode to get multiple values to insert until the user enters q
+		{
+
+			string value; // empty string
+			while (value != "q")
+			{
+				Employee temp;
+
+				temp.setEmpCode("S");
+
+				cout << "Enter an SSN : ";
+				cin >> value;
+				temp.setSSN(value);
+
+				string first, last;
+				cout << "Enter an first name: ";
+				cin >> first;
+
+				cout << "Enter an last name: ";
+				cin >> last;
+				temp.setName(first, last);
+
+				cout << "Enter an department: ";
+				cin >> value;
+				temp.setDept(value);
+
+				cout << "Enter an role: ";
+				cin >> value;
+				temp.setRole(value);
+
+				cout << "Enter an salary: ";
+				cin >> value;
+				temp.setSalary(stoi(value));
+
+				if (value != "q")
+				{
+					bst.insert(temp.getSSN(), temp);
+					bst.print();
+				}
 
 				cout << "Enter an q to quit, if not, enter any key to contuine. " << endl;
-                cin >> value;
-            }
-        }
-        else if (input == "r")
-        {
-        }
-        else if (input == "c")
-        {
-        }
-        else if (input == "t")
-        {
-        }
-        else if (input == "h")
-        {
-        }
-        else if (input == "s")
-        {
-        }
-        else if (input == "m")
-        {
-           // bst.preorder(); // increase the value of every node
-            bst.print();
-        }
-        else if (input == "n")
-        {
-        }
-        else if (input == "p")
-        {
-        }
-        else if (input == "l")
-        {
-        }
-        else if (input == "d")
-        {
-        }
-        else if (input == "x")
-        {
-            exit_now = true;
-        }
+				cin >> value;
+			}
+		}
+		else if (input == "r")
+		{
+			string key;
+			string value;
 
-        // complete this code to handle all the remaining menu items
-    }
+			while (value != "q")
+			{
+				cout << "Enter the SSN you would like to delete. ";
+				cin >> key;
+				bst.erase(key);
+				bst.print();
+
+				cout << "Enter an q to quit, if not, enter any key to contuine. " << endl;
+				cin >> value;
+			}
+		}
+		else if (input == "c")
+		{
+			string value;
+			cout << "Are you sure? This will remove all data. Press y to contuine, press q to quit." << endl;
+			cin >> value;
+
+			if (value == "y")
+			{
+				bst.clear();
+				cout << "All data has been removed" << endl;
+			}
+			else
+			{
+			}
+		}
+		else if (input == "t")
+		{
+			bst.print();
+			cout << "Tree has been printed" << endl;
+		}
+		else if (input == "h")
+		{
+			int height = bst.height();
+			cout << "The height of the tree is " << height << " " << endl;
+		}
+		else if (input == "s")
+		{
+			int size = bst.size();
+			cout << "The size of the tree is " << size << endl;
+		}
+		else if (input == "m")
+		{
+			bst.preorder(printEmployee);
+			bst.print();
+		}
+		else if (input == "n")
+		{
+			bst.inorder(printEmployee);
+			bst.print();
+		}
+		else if (input == "p")
+		{
+			bst.postorder(printEmployee);
+			bst.print();
+		}
+		else if (input == "l")
+		{
+			int count = bst.leaf_count();
+			cout << "Number of leaves: " << count << endl;
+		}
+		else if (input == "d")
+		{
+		}
+		else if (input == "x")
+		{
+			exit_now = true;
+		}
+		else
+		{
+			cout << endl
+				 << "Invaild input, try again." << endl;
+		}
+	}
 }
 
 void menu()
 {
-    cout << "======== Menu ========" << endl;
-    cout << "i. Insert" << endl; // until the user enters "q<Enter" to quit the insert mode
-    cout << "r. Remove" << endl; // until the user enters "q<Enter" to quit the remove mode
-    cout << "c. Clear tree" << endl;
-    cout << "t. Print tree" << endl;
-    cout << "h. Print tree height" << endl;
-    cout << "s. Print tree size" << endl;
-    cout << "m. Pre-order traversal" << endl;
-    cout << "n. In-order traversal" << endl;
-    cout << "p. Post-order traversal" << endl;
-    cout << "l. Leaf count" << endl;
-    cout << "d. operator=()" << endl;
-    cout << "x. Exit" << endl;
-    cout << "======== Menu ========" << endl;
+	cout << "======== Menu ========" << endl;
+	cout << "i. Insert" << endl; // until the user enters "q<Enter" to quit the insert mode
+	cout << "r. Remove" << endl; // until the user enters "q<Enter" to quit the remove mode
+	cout << "c. Clear tree" << endl;
+	cout << "t. Print tree" << endl;
+	cout << "h. Print tree height" << endl;
+	cout << "s. Print tree size" << endl;
+	cout << "m. Pre-order traversal" << endl;
+	cout << "n. In-order traversal" << endl;
+	cout << "p. Post-order traversal" << endl;
+	cout << "l. Leaf count" << endl;
+	cout << "d. operator=()" << endl;
+	cout << "x. Exit" << endl;
+	cout << "======== Menu ========" << endl;
 }
